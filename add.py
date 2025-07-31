a51ec3c606878dc4ce8f1aa30bee1ad49281cff5
umasri-26 <umasriyannam78@gmail.com>
2025-07-31 15:40:38 +0530

    program to add two nubers

---
+++ add.py
@@ -0,0 +1,14 @@
+#write apython program for adding two numbers
+def add_numbers(num1, num2):
+    """Function to add two numbers."""
+    return num1 + num2
+#code to add two numbers
+# Example usage
+if __name__ == "__main__":
+    a = 5
+    b = 3
+    result = add_numbers(a, b)
+    print("Sum:", result)
+
+
+
